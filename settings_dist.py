0a66110d887d35dcc6f4bcbd79ddc1a1a0ad79e8
Azzeccagarbugli <f.coppola1998@gmail.com>
2017-09-17 00:10:35 +0200

    [UPDATE] Aggiornato messaggio di start

--- settings_dist.py
+++ settings_dist.py
@@ -5,8 +5,9 @@ API = "API"
 TOKEN = "TOKEN"
 
 # Start message
-start_msg = "Benvenuto su Kedath! Digita il comando /set_username per settare il tuo username e iniziare a ricevere aggioramenti a game finito, "\
+start_msg = "Benvenuto su Kedath! Digita il comando /set_summoner per settare il tuo username e il "\
+            "tuo server di appartenenza e iniziare a ricevere aggioramenti a game finito, "\
             "oppure digita il comando /help per ricevere più informazioni riguardo il funzionamento del Bot"
-
+            
 # Help message 
 help_msg = "JUST PAOLOCANNONE STYLE 22"
