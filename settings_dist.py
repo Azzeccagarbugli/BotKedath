aab0fafab677a8d9575615a172fd7b9be9dbcbd7
Azzeccagarbugli <f.coppola1998@gmail.com>
2017-09-17 18:56:15 +0200

    [UPDATE] Aggiunto nuovo messaggio di testo

--- settings_dist.py
+++ settings_dist.py
@@ -11,3 +11,8 @@ start_msg = "Benvenuto su Kedath! Digita il comando /set_summoner per settare il
             
 # Help message 
 help_msg = "JUST PAOLOCANNONE STYLE 22"
+
+## KDA message notification
+ad_msg = "Da questo momento in poi riceverai una notifica ogni volta che il tuo game sarà concluso "\
+         "che ti avviserà con delle statistiche riguardo il tuo KDA, per disabilitare questa opzione "\
+         "puoi usare il comando /silence_kda in modo tale da non ricevere la notifica a fine game"
